3961af1a74a8fe91aec9bac09ab8ee75bce34f65
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2022-06-16 20:00:48 -0300

    clase 12

---
+++ clase12/objetos-anidados.js
@@ -0,0 +1,45 @@
+let pokemon = {
+    nombre: 'Pikachu',
+    tipo: 'Electrico',
+    pokedex: 25,
+    entrenador: {
+        nombre: 'Ash',
+        origen: 'Pueblo Paleta',
+        ligas: 1,
+        edad: 10,
+    },
+    movimientos: [
+        {
+            nombre: 'Impactrueno',
+            tipo: 'electrico',
+            poder: 40,
+            precision: 80,
+            usos: 10,
+        },
+        {
+            nombre: 'Tacle',
+            tipo: 'normal',
+            poder: 40,
+            precision: 80,
+            usos: 20,
+        },
+        {
+            nombre: 'Gruñido',
+            tipo: 'normal',
+            poder: 0,
+            precision: 100,
+            usos: 30,
+        },
+        {
+            nombre: 'Cola ferrea',
+            tipo: 'acero',
+            poder: 40,
+            precision: 80,
+            usos: 10,
+        }
+    ]
+}
+
+//1- Console.log de el nombre y edad del entrenador de Pikachu.
+//2- Modificar el valor de precisión del movimiento de nombre tacle en 100.
+//3- Eliminar el ultimo movimiento utilizando la función pop, y reemplazarlo utilizando la función push por uno nuevo.
